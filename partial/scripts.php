219783fc2bcf139feea27c33b6a56d002543d37d
maam27 <martkools@live.nl>
2018-04-23 09:41:02 +0200

    update indeling

---
+++ partial/scripts.php
@@ -0,0 +1,4 @@
+<script src="js/jquery-3.3.1.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
