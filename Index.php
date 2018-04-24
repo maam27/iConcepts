df22e5023362bd71d39324aa322cf8df7a99faf2
Cry4mercy <Ryanpullen95@gmail.com>
2018-04-24 09:48:34 +0200

    hallo

--- Index.php
+++ Index.php
@@ -44,7 +44,7 @@
             <div class="jumbotron">
                 <div class="container">
                     <h1 class="display-3">Hello, world!</h1>
-                    <p>imaninaary or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                    <p>imaninary or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
                     <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
                 </div>
             </div>
