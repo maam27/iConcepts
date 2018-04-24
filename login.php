8e3ed574e13f8a05e716a05575dfcad2a0ddfc90
BoarBoarBoar <bcv_neo@hotmail.com>
2018-04-24 12:14:06 +0200

    Login formulier + registratie

--- login.php
+++ login.php
@@ -20,15 +20,16 @@ include_once 'partial/menu.php';
 
 <main>
     <div class="container">
+        <div class="loginRegistreerSection text-left">
         <h1>Login</h1>
         <form method="Post" action="#">
-            <input id="username" name="username" type="email"        placeholder="E-mail"    required    value="<?=if_set('username','post')?>">
+            <input id="username" name="username" type="email"        placeholder="E-mail"    required><br><br>
             <input id="password" name="password" type="password"    placeholder="Wachtwoord"  required>
             <button>login</button>
         </form>
-        <p>Heeft u nog geen account klik <a class="no-margin" href="abonnementen.php">hier</a> om naar registratie te gaan</p>
+        <p class="onderkantLoginRegistreer">Heeft u nog geen account klik <a class="no-margin" href="registreer.php">hier</a> om naar registratie te gaan</p>
+        </div>
 
-    </div>
 </main>
 
 <footer class="container">
--- registreer.php
+++ registreer.php
@@ -1,7 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Jumbotron Template for Bootstrap Lool</title>
+    <?php
+    require_once 'partial/styles.php';
+    ?>
+</head>
+
+<body>
 <?php
-/**
- * Created by PhpStorm.
- * User: Bonno
- * Date: 24-4-2018
- * Time: 11:25
- */
+include_once 'partial/menu.php';
+?>
+
+<main>
+    <div class="container">
+        <div class="loginRegistreerSection text-left">
+            <h1>Registreer</h1>
+            <form method="Post" action="#">
+                <p>E-Mail:</p>
+                <input id="username" name="username" type="email"        placeholder="E-mail"    required>
+                <p>Wachtwoord:</p>
+                <input id="password" name="password" type="password"    placeholder="Wachtwoord"  required>
+                <p>Ander ding1:</p>
+                <input id="username" name="username" type="email"        placeholder="E-mail"    required>
+                <p>Ander ding2:</p>
+                <input id="password" name="password" type="password"    placeholder="Wachtwoord"  required>
+                <p>Ander ding3:</p>
+                <input id="username" name="username" type="email"        placeholder="E-mail"    required>
+                <p>Ander ding4:</p>
+                <input id="password" name="password" type="password"    placeholder="Wachtwoord"  required>
+                <br>
+
+                <button>login</button>
+            </form>
+            <p class="onderkantLoginRegistreer">Heeft u al een account klik <a class="no-margin" href="login.php">hier</a> om naar inloggen te gaan</p>
+        </div>
+
+</main>
+
+<footer class="container">
+    <p>&copy; Company 2017-2018</p>
+</footer>
+<?php
+include_once 'partial/scripts.php';
+?>
+</body>
+</html>
