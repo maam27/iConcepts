b337d15b86b715c734ddee97d94003559d7b5432
maam27 <martkools@live.nl>
2018-04-26 11:07:43 +0200

    v1 login

--- login.php
+++ login.php
@@ -9,24 +9,19 @@ require_once 'php/user_functions.php';
 <?php
 include_once 'partial/menu.php';
 
-$message ='';
+$attemptedLogin = false;
 if(isset($_POST)){
-    if(isset($_POST['username']) && isset($_POST['password'])){
+    if(isset($_POST['username']) && isset($_POST['password'])) {
+        $attemptedLogin = true;
         $pass = $_POST['password'];
-        //validate user and pass
-        if(!preg_match('/[A-Z]/', $pass) || !preg_match('/[0-9]/', $pass)){
-            $message .= 'foutmelding';
-        }
-
-        if(empty($message)){
-            $pass = $_POST['password']; //md5($_POST['password']);
-            if(login_user($db,$_POST['username'],$pass)){
-                redirect("index.php");
+        if (preg_match('/[A-Z]/', $pass) && preg_match('/[0-9]/', $pass)) {
+            if (empty($message)) {
+                $pass = $_POST['password']; //md5($_POST['password']);
+                if (login_user($db, $_POST['username'], $pass)) {
+                    redirect("index.php");
+                }
             }
-        }else{
-            echo $message;
         }
-
     }
 }
 ?>
@@ -34,26 +29,36 @@ if(isset($_POST)){
 <main>
     <div class="container login">
         <form method="Post" action="#">
-            <div class="row login-register-section">
+            <div class="row login-section">
                 <div class="col-12">
                     <h1>Login</h1>
                 </div>
-                    <div class="col-12 col-sm-6">
-                        <p>Gebruikersnaam:</p>
-                    </div>
-                    <div class="col-12 col-sm-6">
-                        <input id="username" name="username" type="text"        placeholder="Gebruikersnaam"    required     value="<?php echo if_set('username','post'); ?>"   >
-                    </div>
-                    <div class="col-12 col-sm-6">
-                        <p>Wachtwoord:</p>
-                    </div>
-                    <div class="col-12 col-sm-6">
-                        <input id="password" name="password" type="password"    placeholder="Wachtwoord"        required minlength="8">
-                    </div>
-                    <div class="col-12">
-                        <button class="button">login</button>
-                    </div>
-                <p class="bottom-login-register-form">Heeft u nog geen account klik <a class="no-margin" href="registreer.php">hier</a> om te registreren.</p>
+                <?php
+                if($attemptedLogin){
+                ?>
+                <div class="col-12 error-message">
+                    <i class="fas fa-exclamation-circle"></i>
+                    De combinatie van e-mailadres en wachtwoord is niet geldig.
+                </div>
+                <?php } ?>
+                <div class="col-12 col-sm-6">
+                    <p>Gebruikersnaam:</p>
+                </div>
+                <div class="col-12 col-sm-6">
+                    <input id="username" name="username" type="text"        placeholder="Gebruikersnaam"    required     value="<?php echo if_set('username','post'); ?>"   >
+                </div>
+                <div class="col-12 col-sm-6">
+                    <p>Wachtwoord:</p>
+                </div>
+                <div class="col-12 col-sm-6">
+                    <input id="password" name="password" type="password"    placeholder="Wachtwoord"        required minlength="8">
+                </div>
+                <div class="col-12">
+                    <button class="btn btn-primary float-right">login</button>
+                </div>
+                <div class="col-12">
+                    <p class="bottom-login-register-form">Heeft u nog geen account klik <a class="no-margin" href="registreer.php">hier</a> om te registreren.</p>
+                </div>
             </div>
         </form>
     </div>
