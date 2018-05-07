fc32d1167fc04d1d34d16b6bd0a07dffa85dcde8
BoarBoarBoar <bcv_neo@hotmail.com>
2018-05-07 10:33:34 +0200

    Inloggen/registreren wachtwoord restrictie
    Toevoeging van een restrictie aan het form voor het wachtwoord. Er kan niet gesubmit worden zonder dat het wachtwoord 1 cijfer, hoofdletter en kleine letter bevat.

--- registreer.php
+++ registreer.php
@@ -5,6 +5,7 @@
 <?php
 require_once 'partial/page_head.php';
 require_once 'php/user_functions.php';
+require_once 'php/generic_functions.php';
 ?>
     <title>Jumbotron Template for Bootstrap Lool</title>
     </head>
@@ -19,6 +20,12 @@ $data1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $melding = '';
 
+if(!empty($_POST['username']) AND !empty($_POST['password']) AND !empty($_POST['password2']) AND !empty($_POST['first-name'])
+    AND !empty($_POST['last-name']) AND !empty($_POST['birth-date']) AND !empty($_POST['e-mail']) AND !empty($_POST['country'])
+    AND !empty($_POST['city']) AND !empty($_POST['address-field']) AND !empty($_POST['postcode']) AND !empty($_POST['security-question'])
+    AND !empty($_POST['answer'])
+)
+{
 if( email_exists($db, $_POST['e-mail']) ){
     $melding = 'Het opgegeven mail-adres is al in gebruik.';
 }
@@ -27,17 +34,11 @@ else if($_POST['password'] != $_POST['password2']){
     $melding = 'Het wachtwoord komt niet overeen met de verificatie.';
 }
 
-else if(!empty($_POST['username']) AND !empty($_POST['password']) AND !empty($_POST['password2']) AND !empty($_POST['first-name'])
-    AND !empty($_POST['last-name']) AND !empty($_POST['birth-date']) AND !empty($_POST['e-mail']) AND !empty($_POST['country'])
-    AND !empty($_POST['city']) AND !empty($_POST['address-field']) AND !empty($_POST['postcode']) AND !empty($_POST['security-question'])
-    AND !empty($_POST['answer'])
-)
-{
-register_user_test($db, $_POST['username'], $_POST['first-name'],$_POST['last-name'],$_POST['address-field'],
+else {register_user_test($db, $_POST['username'], $_POST['first-name'],$_POST['last-name'],$_POST['address-field'],
     $_POST['address-field2'],$_POST['postcode'], $_POST['city'],$_POST['country'],$_POST['birth-date'],$_POST['e-mail'],
     $_POST['password'],$_POST['security-question'], $_POST['answer']);
 }
-
+}
 
 
 
@@ -53,47 +54,46 @@ register_user_test($db, $_POST['username'], $_POST['first-name'],$_POST['last-na
                 <form method="Post" action="#">
                     <div class="row">
                         <div class="col-md-6">
-                            <?php
-                            if(isset($melding)){echo '<p>' . $melding . '</p>';}
-                            ?>
                             <p>Gebruikersnaam:</p>
-                            <input id="username" name="username" type="text" placeholder="BarryBadpak" value="Kutzooi123" required>
+                            <input id="username" name="username" type="text" placeholder="BarryBadpak" <?php echo post_set('username', 'post');?> required>
                             <br>
                             <p>Wachtwoord:</p>
-                            <input id="password" name="password" type="password" placeholder="Wachtwoord" value="teringwachtwoord" required>
+                            <input id="password" name="password" type="password" placeholder="Wachtwoord" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                   title="Het wachtwoord moet minimaal 1 nummer, 1 hoofdletter en 1 kleine letter bevatten. Minimum aantal karakters is 8." required>
                             <br>
                             <p>Bevestig wachtwoord:</p>
-                            <input id="password2" name="password2" type="password" placeholder="Wachtwoord123!" value="teringwachtwoord"
-                                   required>
+                            <input id="password2" name="password2" type="password" placeholder="Wachtwoord" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                   title="Het wachtwoord moet minimaal 1 nummer, 1 hoofdletter en 1 kleine letter bevatten. Minimum aantal karakters is 8." required>
                             <br>
                             <p>Voornaam:</p>
-                            <input id="first-name" name="first-name" type="text" placeholder="Barry" value="Rotnaam" required>
+                            <input id="first-name" name="first-name" type="text" placeholder="Barry" <?php echo post_set('first-name', 'post');?> required>
                             <br>
                             <p>Achternaam:</p>
-                            <input id="last-name" name="last-name" type="text" placeholder="Badpak" value="LeukeAchternaam" required>
+                            <input id="last-name" name="last-name" type="text" placeholder="Badpak" <?php echo post_set('last-name', 'post');?> required>
                             <br>
                             <p>Geboortedatum:</p>
-                            <input id="birth-date" name="birth-date" type="date"  required>
+                            <input id="birth-date" name="birth-date" type="date" <?php echo post_set('birth-date', 'post');?> required>
                             <br>
                             <p>E-Mail:</p>
-                            <input id="e-mail" name="e-mail" type="email" placeholder="E-mail" value="Rotformulier@mail" required>
+                            <input id="e-mail" name="e-mail" type="email" placeholder="E-mail" <?php echo post_set('e-mail', 'post');?> required>
                             <br>
                         </div>
                         <div class="col-md-6">
+
                             <p>Land:</p>
-                            <input id="country" name="country" type="text" placeholder="Nederland" value="BoeitNiet" required>
+                            <input id="country" name="country" type="text" placeholder="Nederland" <?php echo post_set('country', 'post');?> required>
                             <br>
                             <p>Stad:</p>
-                            <input id="city" name="city" type="text" placeholder="Arnhem" value="Stad123" required>
+                            <input id="city" name="city" type="text" placeholder="Arnhem" <?php echo post_set('city', 'post');?> required>
                             <br>
                             <p>Adresregel 1:</p>
-                            <input id="address-field" name="address-field" type="text" placeholder="Straatnaam 15" value="Adress 14" required>
+                            <input id="address-field" name="address-field" type="text" placeholder="Straatnaam 15" <?php echo post_set('address-field', 'post');?> required>
                             <br>
                             <p>Adresregel 2:</p>
-                            <input id="address-field2" name="address-field2" type="text" placeholder="Toevoeging adresregel">
+                            <input id="address-field2" name="address-field2" type="text" placeholder="Toevoeging adresregel" <?php echo post_set('address-field2', 'post');?>>
                             <br>
                             <p>Postcode: </p>
-                            <input id="postcode" name="postcode" type="text" placeholder="4323DK" value="4232WK" required>
+                            <input id="postcode" name="postcode" type="text" placeholder="4323DK" <?php echo post_set('postcode', 'post');?> required>
                             <br>
                             <p>Veiligheidsvraag: </p>
                             <select name="security-question">
@@ -105,12 +105,15 @@ register_user_test($db, $_POST['username'], $_POST['first-name'],$_POST['last-na
                             </select>
                             <br>
                             <p>Antwoord:</p>
-                            <input id="answer" name="answer" type="text" placeholder="Antwoord" value="Nee" required>
+                            <input id="answer" name="answer" type="text" placeholder="Antwoord"  required>
                             <br>
                             <button type="submit">Registreer</button>
                         </div>
                     </div>
                 </form>
+                <?php
+                if(isset($melding)){echo '<p class="error-message">' . $melding . '</p>';}
+                ?>
                 <p class="bottom-login-register-form">Heeft u al een account klik <a class="no-margin" href="login.php">hier</a>
                     om naar inloggen te gaan</p>
             </div>
