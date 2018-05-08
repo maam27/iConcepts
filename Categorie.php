dde92f476ae9ed9f07afd87d5eff73dcca9f5a60
Cry4mercy <Ryanpullen95@gmail.com>
2018-05-08 14:48:55 +0200

    database integration in process

--- Categorie.php
+++ Categorie.php
@@ -17,9 +17,19 @@
 <body>
 <?php
 include_once 'partial/menu.php';
+
+$Rubriek = isset($_GET['Rubrieknaam']) ? $_GET['Rubrieknaam'] : '';
 ?>
 
 <main>
+<!--    SideNavigation Bar    -->
+
+    <?php
+    $query = $db->prepare("SELECT [rubrieknaam] FROM Rubriek ");
+    $query->execute(array(':rubrieknaam' => $Rubriek));
+    $result = $query->fetch();
+
+    $Categorie.=<<<rubriek
     <div class="container-fluid">
         <div class="row">
             <div class="sidebar1">
@@ -31,25 +41,22 @@ include_once 'partial/menu.php';
                         <li>Afstand</li>
                         <li>Staat</li>
                         <li>Sorteren op</li>
-                        <li>Leeg filter</li>
-                        <li>Leeg Filter</li>
                     </ul>
 
                     <br>
 
                     <ul class="list">
-                        <h5><strong>Alle categorieën </strong></h5>
-                        <li>categorie 1</li>
-                        <li>Categorie 2</li>
-                        <li>Categorie 3</li>
-                        <li>Categorie 4</li>
-                        <li>Categorie 5</li>
-                        <li>Categorie 6</li>
+                    <h5><strong>Alle categorieën </strong></h5>
+                   <?php foreach($Rubriek as $key=>$value){
+                        <li><a href="categorie.php?id=$Rubriek[$key] ['Rubrieknaam'];"></a></li>
+                     } 
+                     
                     </ul>
                 </div>
             </div>
+
             <div class="col-md-10 col-sm-8 main-content">
-                <!--Main content code to be written here -->
+                <!--    Main content    -->
                 <br><br><br>
                     <div class="row">
                         <h2>Categorie: TRUMP</h2>
@@ -63,7 +70,35 @@ include_once 'partial/menu.php';
                         </div>
                     </div>
                     <div class="auction-section">
+rubriek;
+    echo $Categorie;
+?>
 
+    <!--    advertentie Sectie   -->
+
+                <table style="width:100%">
+                    <tr>
+                        <th><img src="images/thumb/placeholder.jpg"  class="auction-thumbnail"/></th>
+                        <th><p>hier kan een product naam of titel komen,maar komt hier dan ook de juiste informatie bij de afbeelding?</p></th>
+                        <th><p> prijs €300</p></th>
+                    </tr>
+                </table>
+
+</main>
+
+
+
+<footer class="container rubriekfooter">
+    <p>&copy; Company 2017-2018</p>
+</footer>
+<?php
+include_once 'partial/scripts.php';
+?>
+</body>
+</html>
+
+
+                                    <!-- Expirimental Code, not applicable -->
 
 
 <!--                        --><?php //foreach($Voorwerp as $key=>$value): ?>
@@ -78,38 +113,3 @@ include_once 'partial/menu.php';
 <!--                                    </table><img src="--><?php //echo "images/thumb/" .$Voorwerp[$key] ['image']; ?><!--" alt="--><?php //echo $Voorwerp[$key] ['image']; ?><!--"/></a>-->
 <!--                            </div>-->
 <!--                        --><?php //endforeach; ?>
-
-                                        <table style="width:100%">
-                                            <tr>
-                                                <th><img src="images/thumb/placeholder.jpg"  class="auction-thumbnail"/></th>
-                                                <th><p>hier kan een product naam of titel komen,maar komt hier dan ook de juiste informatie bij de afbeelding?</p></th>
-                                                <th><p> prijs €300</p></th>
-                                            </tr>
-                                        </table>
-
-                    </div>
-                </div>
-            </div>
-
-
-
-<!--        <div class="row auction-section">-->
-<!--            <div class="col-sm-12 col-xl-3 col-md-5">-->
-<!--               <img src="images/thumb/placeholder.jpg"  class="auction-thumbnail"/>-->
-<!--            </div>-->
-<!--            <div class="col-sm-12 col-xl-7 col-md-9">-->
-<!--                <p>hier kan een product naam of titel komen,maar komt hier dan ook de juiste informatie bij de afbeelding?</p></div>-->
-<!---->
-<!--            <div class="col-xl-2 col-sm-12 col-md-6">  <p> prijs €300 </p></div>-->
-<!--            </div>-->
-<!--        </div>-->
-
-
-<footer class="container rubriekfooter">
-    <p>&copy; Company 2017-2018</p>
-</footer>
-<?php
-include_once 'partial/scripts.php';
-?>
-</body>
-</html>
