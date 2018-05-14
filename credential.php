e36f01ac005d1cbfe257431b280c4f0aeca3e517
jim <jimmy_ajax_verwoert@hotmail.com>
2018-05-14 09:58:00 +0200

    panda

--- credential.php
+++ credential.php
@@ -3,6 +3,6 @@ $hostname = "mssql2.iproject.icasites.nl";
 $dbname = "iproject14";
 $username = "iproject14";
 $pw = "	e5aQSbD2uH";
-$dbh = new PDO ("sqlsrv:Server=localhost;Database=$dbname;
+$dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;
             ConnectionPooling=0", "$username", "$pw");
 ?>
