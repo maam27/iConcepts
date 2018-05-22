ba968f42141da0af57ad50f9ad1eb1ace06b9ec9
maam27 <martkools@live.nl>
2018-05-22 15:15:53 +0200

    Merge branch 'development' of https://github.com/maam27/iConcepts into development
    # Conflicts:
    #	css/iconcepts.css

--- Categorie.php
+++ Categorie.php
@@ -55,64 +55,72 @@ $categorie = isset($_GET['rubriek']) ? $_GET['rubriek'] : '';
 
 ?>
 
-    <!--    SideNavigation Bar    -->
-<div class="categorie-content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="sidebar1">
-
-                <div class="left-navigation">
+<div class="container">
+    <div class="row">
+        <div class="col-2" style="background-color: #F2552C;">
+            <div class="row">
+                <div class="col-12">
+                    <h5><strong>filters</strong></h5>
+                </div>
+                <div class="col-12">
                     <ul class="list">
-                        <h5><strong>filters</strong></h5>
                         <li>Prijs</li>
                         <li>Afstand</li>
                         <li>Staat</li>
                         <li>Sorteren op</li>
                     </ul>
-
-                    <br>
-
+                </div>
+                <div class="col-12">
+                    <h5><strong> Alle categorieën </strong></h5>
+                </div>
+                <div class="col-12">
                     <ul class="list">
-                        <h5><strong> Alle categorieën </strong></h5>
                         <?php foreach($Rubriek as $row ):?>
                             <li> <a href="categorie.php?rubriek=<?php echo $row ['Rubrieknaam'];?>"  > <?php echo $row ['Rubrieknaam'];?></a></li>
                         <?php endforeach;?>
-
-
                     </ul>
                 </div>
+
+
+            </div>
+
+
+        </div>
+        <div class="col-10">
+            <div class="row">
+                <?php foreach($Artikelen as $kavel ): ?>
+                    <div class="col-3">
+                        <img src="http://iproject14.icasites.nl/<?php echo get_image_path( $kavel['Filenaam'],  false);?>"  class="auction-thumbnail"/>
+                    </div>
+                <div class="col-6">
+                    <p><?php echo $kavel ['Voorwerpnummer']; ?> &nbsp <?php echo $kavel['Titel'];?></p>
+                </div>
+
+
+                    <div class="col-3">
+                        <p> Start prijs <?php echo $kavel ['Startprijs'];?></p>
+                    </div>
+                <?php endforeach; ?>
+
             </div>
         </div>
 
-        <!--    Main content    -->
-        <br><br><br>
-            <div class="row">
-                <h2>  Categorie: <?php echo $categorie ;?></h2>
-                <div class="col-12">
-                    <div class="d-flex justify-content-around flex-wrap">
-                        <div class="" style="background:salmon;width:200px;">altijd<img src="images/thumb/placeholder.jpg" class="img-thumbnail"/></div>
-                        <div class="" style="background:salmon;width:200px;">altijd<img src="images/thumb/placeholder.jpg" class="img-thumbnail"/></div>
-                        <div class="d-none d-md-block" style="background:salmon;width:200px;">tablet+<img src="images/thumb/placeholder.jpg" class="img-thumbnail"/></div>
-                        <div class="d-none d-lg-block" style="background:salmon;width:200px;">laptop+<img src="images/thumb/placeholder.jpg" class="img-thumbnail"/></div>
-                    </div>
-                </div>
-            </div>
+    </div>
+</div>
+
+
+
+
+    <!--    SideNavigation Bar    -->
+<div class="categorie-content">
+    <div class="container-fluid">
 
 
             <!--    advertentie Sectie   -->
 
         <div class="auction-section">
            <table style="width:100%">
-                <?php foreach($Artikelen as $kavel ):?>
-                <tr>
-                    <th><p><?php echo $kavel ['Voorwerpnummer']; ?> &nbsp <?php echo $kavel['Titel'];?></p></th>
-                </tr>
-                <tr>
-                    <th><img src="http://iproject14.icasites.nl/<?php echo get_image_path( $kavel['Filenaam'],  false);?>"  class="auction-thumbnail"/></th>
 
-                    <th><p> Start prijs <?php echo $kavel ['Startprijs'];?></p></th>
-                </tr>
-                <?php endforeach; ?>
            </table>
         </div>
     </div>
