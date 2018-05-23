96fe9e1eaff528d3f9a29da08071bd75b67ca272
maam27 <martkools@live.nl>
2018-05-23 11:02:07 +0200

    timer

--- Categorie.php
+++ Categorie.php
@@ -80,56 +80,42 @@ $categorie = isset($_GET['rubriek']) ? $_GET['rubriek'] : '';
                         <?php endforeach;?>
                     </ul>
                 </div>
-
-
             </div>
-
-
         </div>
         <div class="col-10">
             <div class="row">
-                <?php foreach($Artikelen as $kavel ): ?>
-                    <div class="col-3">
-                        <img src="http://iproject14.icasites.nl/<?php echo get_image_path( $kavel['Filenaam'],  false);?>"  class="auction-thumbnail"/>
-                    </div>
-                <div class="col-6">
-                    <p><?php echo $kavel ['Voorwerpnummer']; ?> &nbsp <?php echo $kavel['Titel'];?></p>
+                <div class="col-12">
+                    <?php foreach($Artikelen as $kavel ): ?>
+                        <div class="row margin-top productblock-large">
+                            <div class="col-3">
+                                <img src="http://iproject14.icasites.nl/<?php echo get_image_path( $kavel['Filenaam'],  false);?>"  class="img-fluid"/>
+                            </div>
+                            <div class="col-9">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <strong><?php echo $kavel['Titel'];?></strong>
+                                    </div>
+                                    <div class="col-4">
+                                        <span class="timer float-right" data-auctionEnd="<?php echo $kavel['LooptijdeindeDag']." ". $kavel['LooptijdeindeTijdstip']; ?>">00:00:00 resterend</span>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-8 no-overflow">
+                                        <?php echo $kavel['Beschrijving'];?></p>
+                                    </div>
+                                    <div class="col-4">
+                                        <span class="float-right">prijs <?php echo $kavel ['Startprijs'];?></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
-
-
-                    <div class="col-3">
-                        <p> Start prijs <?php echo $kavel ['Startprijs'];?></p>
-                    </div>
-                <?php endforeach; ?>
-
             </div>
         </div>
-
     </div>
 </div>
-
-
-
-
-    <!--    SideNavigation Bar    -->
-<div class="categorie-content">
-    <div class="container-fluid">
-
-
-            <!--    advertentie Sectie   -->
-
-        <div class="auction-section">
-           <table style="width:100%">
-
-           </table>
-        </div>
-    </div>
-</div>
-
 </main>
-
-
-
 
 <?php
 require_once 'partial/page_footer.php';
@@ -138,4 +124,40 @@ include_once 'partial/scripts.php';
 </body>
 </html>
 
+<script>
+    window.setInterval(function(){
+        $(".timer").each(function(){
+            $endTime = new Date($(this).data('auctionend'));
+            //some have a diffrent timezone, thus lasting 1 hour longer/shorter
+            $currentTime = new Date($.now());
+            $timeDiffrence = $endTime - $currentTime;
 
+            if ($timeDiffrence <= 0) {
+                $(this).text("Verlopen");
+                $(this).removeClass( "timer" )
+            }
+            else {
+                $sec = Math.floor($timeDiffrence / 1000);
+                $min = Math.floor($sec / 60);
+                $hour = Math.floor($min / 60);
+                $days = Math.floor($hour / 24);
+
+                $hour %= 24;
+                $min %= 60;
+                $sec %= 60;
+
+                uren = ("0" + $hour).slice(-2);
+                minuten = ("0" + $min).slice(-2);
+                $sec = ("0" + $sec).slice(-2);
+
+                if($days >= 2){
+                    $(this).text($days + " dagen");
+                }else {
+                    $(this).text(($days * 24 + $hour) + ":" + $min + ":" + $sec);
+                }
+            }
+                //eind test
+        });
+
+    }, 1000);
+</script>
